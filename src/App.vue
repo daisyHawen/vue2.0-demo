1dcf6fa9cd321d7a939b358b2815e801454e2f0b
daisyHawen <512309453@qq.com>
2016-10-10 16:48:22 +0800

    add all

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello.vue'
+// export default {
+//   data () {
+//     return {
+//       msg: 'Hello Vue!'
+//     }
+//   }
+// }
+
+export default {
+ components:{Hello}
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
